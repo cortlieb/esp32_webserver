b2ebab00747478cd7d6819678775ce83c640ca8c
Christian Ortlieb <info@ortliebweb.com>
2023-09-01 21:41:48 +0200

    feat: prepare code for web server.
    Added ESP Async Web Server library.
    Added code to connect to WiFi and start Webserver.

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,48 @@
 // #include <string>
 // using namespace std;
 
+// #include <Arduino.h> //lt. Webserver Ebook nötig, war aber vorher nicht da und lief bis dahin trotzdem - beobachten!
 #include <SPI.h>
 #include <Adafruit_NeoPixel.h>
 #include "../include/colors.h"
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+// Replace with your network credentials
+const char *ssid = "Villanetz";
+const char *password = "Kellerbad100%";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+<title>ESP Web Server</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+<style>
+html {
+font-family: Arial;
+text-align: center;
+}
+body {
+max-width: 400px;
+margin:0px auto;
+}
+</style>
+</head>
+<body>
+<h1>Hello World!</h1>
+<p>Congratulations!<br>This is your first Web Server with the ESP.</p>
+</body>
+</html>
+)rawliteral";
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -46,35 +82,18 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 Adafruit_NeoPixel ringKlein(LED_RING_KL_COUNT, LED_RING_KL_PIN, NEO_GRB + NEO_KHZ800);
 
-#define NUMFLAKES 10 // Number of snowflakes in the animation example
-
-#define LOGO_HEIGHT 16
-#define LOGO_WIDTH 16
-static const unsigned char PROGMEM logo_bmp[] =
-	{0b00000000, 0b11000000,
-	 0b00000001, 0b11000000,
-	 0b00000001, 0b11000000,
-	 0b00000011, 0b11100000,
-	 0b11110011, 0b11100000,
-	 0b11111110, 0b11111000,
-	 0b01111110, 0b11111111,
-	 0b00110011, 0b10011111,
-	 0b00011111, 0b11111100,
-	 0b00001101, 0b01110000,
-	 0b00011011, 0b10100000,
-	 0b00111111, 0b11100000,
-	 0b00111111, 0b11110000,
-	 0b01111100, 0b11110000,
-	 0b01110000, 0b01110000,
-	 0b00000000, 0b00110000};
-
-
+void initWiFi();
 void drawCycleText(const char *test_char);
 void runningCircleSingle(uint32_t color, uint32_t backgroundColor, int wait);
 
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin(115200);
+	initWiFi();
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+			  { request->send(200, "text/html", index_html); });
+	// Start server
+	server.begin();
 
 	// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
 	if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
@@ -148,4 +167,17 @@ void runningCircleSingle(uint32_t color, uint32_t backgroundColor, int wait)
 		ringKlein.show();
 		delay(wait);
 	}
+}
+
+void initWiFi()
+{
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	Serial.print("Connecting to WiFi ..");
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print('.');
+		delay(1000);
+	}
+	Serial.println(WiFi.localIP());
 }
